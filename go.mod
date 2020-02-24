1b9fae27052c5db537e33cf08846b64d2bd1a352
aibotsoft <aibotsoft@gmail.com>
2020-02-24 14:58:39 +0200

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aibotsoft/go-template
 
 go 1.13
+
+require github.com/jackc/pgx/v4 v4.4.1
--- src/main.go
+++ src/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+)
+
+const DATABASE_URL = "postgres://postgres:postgres@localhost/postgres"
+
+func selectNow(conn *pgx.Conn) time.Time {
+	var name time.Time
+	err := conn.QueryRow(context.Background(), "select now()").Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return name
+}
 func main() {
-	print("Hello World")
+	ctx := context.Background()
+	db, err := pgx.Connect(ctx, DATABASE_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close(ctx)
+	for {
+		timeNow := selectNow(db)
+		log.Println(timeNow)
+		time.Sleep(time.Second)
+	}
 }
